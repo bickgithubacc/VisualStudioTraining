679c989dac507f7e2cddd447f6591c682503487f
bickgithubacc <theresa.b.vu@oregon.gov>
2021-09-27 14:34:45 -0700

    I just add new code in

---
+++ ConsoleApp1/Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1.Extensions
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Create a List of List of T from a List of T split by integer
+        /// </summary>
+        /// <typeparam name="T">Type of List</typeparam>
+        /// <param name="source">List to split</param>
+        /// <param name="chunkSize">Split list by</param>
+        /// <returns>Chunked list</returns>
+        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
+        {
+            return source
+                .Select((value, index) => new { Index = index, Value = value })
+                .GroupBy(item => item.Index / chunkSize)
+                .Select(item => item.Select(value => value.Value).ToList())
+                .ToList();
+        }
+    }
+}
